52c77ae117ac11f361578b3dc6c7ab6f10231c72
streamlineddesigns <streamline.our.designs@gmail.com>
2021-01-24 18:04:32 -0500

    updated descriptors again & models

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -36,10 +36,10 @@
 
         <!-- use rawgithack to retrieve the correct url for nft marker (see 'trex' below) -->
         <a-nft
-            type='nft' url="img/descriptors/logo/logo"
+            type='nft' url="img/descriptors/laurielafrate/laurielafrate"
             smooth='true' smoothCount='10' smoothTolerance='0.01' smoothThreshold='5'>
             <a-entity
-                gltf-model='https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf'
+                gltf-model='models/scene.gltf'
                 scale="5 5 5"
                 position="100 100 0"
                 >
