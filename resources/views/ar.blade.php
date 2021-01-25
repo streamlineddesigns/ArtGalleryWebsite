c66e986413d1fa6f66f1364a8e1145e01587514d
streamlineddesigns <streamline.our.designs@gmail.com>
2021-01-25 00:06:16 -0500

    Update ar.blade.php

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -51,11 +51,11 @@
 
     <script src="vendor/jquery/jquery.min.js"></script>
     <script>
-        $(document).ready(function() {
+        /*$(document).ready(function() {
             var imgHeight = $("#img-dimensions").height();
             var imgWidth = $("#img-dimensions").width();
             $("a-entity").first().attr("position").x = (screen.width / 2) - (imgWidth / 2);;
             $("a-entity").first().attr("position").z = - ((screen.height / 2) - (imgHeight / 2));
-        });
+        });*/
     </script>
 </body>
