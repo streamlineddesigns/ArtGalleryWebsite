7b8ef65d630dfd2f2f4478b9cacc5a3e6ca91686
streamlineddesigns <streamline.our.designs@gmail.com>
2021-02-11 12:51:13 -0500

    organization

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -4,8 +4,8 @@
 <head>
   <x-analytics/>
   <meta charset="utf-8">
-  <title>View Laurie LaFrate's Artwork</title>
-  <meta name="description" content="Extraordinary artwork by Laurie LaFrate for extraordinary residential and commercial spaces – crafted with love, from a studio on New York's Upper East Side.">
+  <title>View Laurie LaFrate's Paintings</title>
+  <meta name="description" content="View original Laurie LaFrate painting's in your home using Augmented Reality!">
 
   <meta property="og:title" content="Laurie LaFrate">
   <meta property="og:type" content="">
@@ -15,58 +15,11 @@
 
 
   <link rel="stylesheet" href="{{url('css/main.css')}}">
+  <link rel="stylesheet" href="{{url('css/ar.css')}}">
   <link rel="stylesheet" href="{{url('vendor/bootstrap/css/bootstrap.min.css')}}">
 
+  <!--aframe-->
   <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-  <style>
-    .arjs-loader {
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      top: 0;
-      left: 0;
-      background-color: rgba(0, 0, 0, 0.8);
-      z-index: 9999;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .arjs-loader div {
-      text-align: center;
-      font-size: 1.25em;
-      color: white;
-    }
-
-    #img-dimensions {
-      display: none;
-    }
-
-    #input-form {
-      position: absolute;
-      height: 200px;
-      width: 200px;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      bottom: 0px;
-      left: 0px;
-      right: 0px;
-      margin: 0 auto;
-      z-index: 100000000;
-    }
-
-    #input-form div label {
-      color: white;
-    }
-
-    #input-form div input {
-      height: 35px;
-      width: 200px;
-      font-size: 22px;
-    }
-  </style>
-
   <!-- rawgithack development URL -->
   <script src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js'></script>
 </head>
@@ -117,14 +70,7 @@
 
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="{{url('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-    <script>
-        $(document).ready(function() {
-          $("a-entity").first().attr("position").x = -60;
-          $("a-entity").first().attr("position").y = -120;
-          $("a-entity").first().attr("position").z = 0;
-          $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
-        });
-    </script>
+    <script src="{{url('js/ar.js')}}"></script>
 </body>
 
 </html>
