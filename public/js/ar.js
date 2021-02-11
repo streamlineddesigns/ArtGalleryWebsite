0022dd7376bb7201f6eaa8b32929926e97fc2b7e
streamlineddesigns <streamline.our.designs@gmail.com>
2021-02-11 13:12:07 -0500

    test

--- public/js/ar.js
+++ public/js/ar.js
@@ -2,7 +2,16 @@ $(document).ready(function() {
     $("a-entity").first().attr("position").x = -60;
     $("a-entity").first().attr("position").y = -120;
     $("a-entity").first().attr("position").z = 0;
-    $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
-
+    
+    //navigation width
     $("#bottom-nav").css("width", screen.width);
+
+    //buttons
+    $(".carousel-control-next").click(function() {
+        $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
+    });
+
+    $(".carousel-control-prev").click(function() {
+        $("a-image").attr("src", "img/Pieces as JPEGS/2-min.jpg");
+    });
 });
