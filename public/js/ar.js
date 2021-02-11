0eb7107b042132611144071d39d8e1c40bce0b4d
streamlineddesigns <streamline.our.designs@gmail.com>
2021-02-11 13:03:24 -0500

    bottom nav

--- public/js/ar.js
+++ public/js/ar.js
@@ -3,4 +3,6 @@ $(document).ready(function() {
     $("a-entity").first().attr("position").y = -120;
     $("a-entity").first().attr("position").z = 0;
     $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
+
+    $("#bottom-nav").css("width", screen.width);
 });
