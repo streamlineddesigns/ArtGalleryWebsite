0a693138076bc2f05005a2ccfb28df13788852fd
streamlineddesigns <streamline.our.designs@gmail.com>
2021-02-11 12:24:11 -0500

    Update ar.blade.php

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -97,7 +97,7 @@
           $("a-entity").first().attr("position").x = -60;
           $("a-entity").first().attr("position").y = -120;
           $("a-entity").first().attr("position").z = 0;
-          $("a-image").attr("src") = "img/Pieces as JPEGS/1-min.jpg";
+          $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
         });
     </script>
 </body>
