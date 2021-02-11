2fb36992774ee3f7eec31922a24091a312fe5e48
streamlineddesigns <streamline.our.designs@gmail.com>
2021-02-11 12:44:19 -0500

    Update ar.blade.php

--- resources/views/ar.blade.php
+++ resources/views/ar.blade.php
@@ -5,52 +5,66 @@
   <x-analytics/>
   <meta charset="utf-8">
   <title>View Laurie LaFrate's Artwork</title>
-  <meta name="description" content="View Laurie LaFrate's painting's in your own house using Augmented Reality!">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <meta property="og:title" content="View Laurie LaFrate's Artwork">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
-  <meta property="og:author" content="Streamlined Designs">
-
   <link rel="stylesheet" href="{{url('css/main.css')}}">
   <link rel="stylesheet" href="{{url('vendor/bootstrap/css/bootstrap.min.css')}}">
 
-  <!--aframe-->
   <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
-  <!--ar css-->
-  <link rel="stylesheet" href="{{url('css/ar.css')}}">
   <style>
     .arjs-loader {
-        height: 100%;
-        width: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-        background-color: rgba(0, 0, 0, 0.8);
-        z-index: 9999;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: rgba(0, 0, 0, 0.8);
+      z-index: 9999;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
 
     .arjs-loader div {
-        text-align: center;
-        font-size: 1.25em;
-        color: white;
+      text-align: center;
+      font-size: 1.25em;
+      color: white;
     }
 
     #img-dimensions {
-        display: none;
+      display: none;
+    }
+
+    #input-form {
+      position: absolute;
+      height: 200px;
+      width: 200px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      bottom: 0px;
+      left: 0px;
+      right: 0px;
+      margin: 0 auto;
+      z-index: 100000000;
+    }
+
+    #input-form div label {
+      color: white;
+    }
+
+    #input-form div input {
+      height: 35px;
+      width: 200px;
+      font-size: 22px;
     }
   </style>
+
   <!-- rawgithack development URL -->
   <script src='https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js'></script>
 </head>
 
 <body style='margin : 0px; overflow: hidden;'>
 
+   <!-- minimal loader shown until image descriptors are loaded -->
   <div class="arjs-loader">
     <div>Loading, please wait...</div>
   </div>
@@ -58,7 +72,20 @@
   <x-header/>
 
     <img src="img/Pieces as JPEGS/16-min.jpg" id="img-dimensions" alt="abstract art">
-    
+    <form id="input-form">
+        <div>
+          <label for="xinput">X Position</label>
+          <input type="text" name="xinput" id="xinput">
+        </div>
+        <div>
+          <label for="yinput">Y Position</label>
+          <input type="text" name="yinput" id="yinput">
+        </div>
+        <div>
+          <label for="zinput">Z Position</label>
+          <input type="text" name="zinput" id="zinput">
+        </div>
+    </form>
     <a-scene
         vr-mode-ui="enabled: false;"
         renderer="logarithmicDepthBuffer: true;"
@@ -81,7 +108,14 @@
 
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="{{url('vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-    <script src="{{url('js/ar.js')}}"></script>
+    <script>
+        $(document).ready(function() {
+          $("a-entity").first().attr("position").x = -60;
+          $("a-entity").first().attr("position").y = -120;
+          $("a-entity").first().attr("position").z = 0;
+          $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
+        });
+    </script>
 </body>
 
 </html>
