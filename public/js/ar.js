378bcba1584528b6acda1394ea810f0701146d9c
streamlineddesigns <streamline.our.designs@gmail.com>
2021-02-11 12:38:47 -0500

    organization

---
+++ public/js/ar.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("a-entity").first().attr("position").x = -60;
+    $("a-entity").first().attr("position").y = -120;
+    $("a-entity").first().attr("position").z = 0;
+    $("a-image").attr("src", "img/Pieces as JPEGS/1-min.jpg");
+});
